25b122465112dd1d6caf7c2b5ef74e26195e6e90
kamaldeep0077 <kamaldp0077@gmail.com>
2023-03-24 17:32:01 +0530

    My new branch commit from developer2 branch

--- README.md
+++ README.md
@@ -3,5 +3,9 @@
 
 ## I will create my own destiny by learning things that ae important 
 ## I will stop wasting my time. 
-## I will be more focused towards the work 
+## I will be more focused towards the work
+
+
+## Adding a new line of comment for testing purposes
+
 
--- app.py
+++ app.py
@@ -7,3 +7,13 @@ for i in range(10):
 
 df = pd.DataFrame({'Col1':[1,2,3,4,5,6,7,8,9,10]})
 print(df.head())
+
+print('This is the change in the new branch that i have just created.')
+
+print('This is the new change to the new branch that i have just created.')
+
+import pandas as pd 
+import numpy as np 
+import gc 
+
+pd.set_options('display.max_columns',None)
